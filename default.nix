121f4d557b3eceafc3ebd1477d97487ba4050dfb
nikp123 <nikp123@e.email>
2024-04-03 19:59:36 +0200

    Add example-package for testing

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   simulide = pkgs.callPackage ./pkgs/simulide { };
+  example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
